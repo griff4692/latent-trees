95ac684bf346c08b10807de2828a485c2d6b77f5
Janani Padmanabhan <aspire.janani@gmail.com>
2017-10-30 16:30:59 -0400

    renaming take 2

--- SPINN/batcher.py
+++ SPINN/batcher.py
@@ -16,11 +16,11 @@ def prepare_snli_batches(args):
     debug_validation = 'mini_dev.jsonl'
     debug_test = 'mini_test.jsonl'
 
-    if not os.path.exists(os.path.join(data_dir, train_path)):
+    if not os.path.exists(os.path.join(data_dir, "snli_1.0_" + train_path)):
         remove_train_unk()
 
     if args.debug:
-        if not os.path.exists(os.path.join(data_dir, debug_train)):
+        if not os.path.exists(os.path.join(data_dir, "snli_1.0_" + debug_train)):
             gen_mini()
 
         print "Using first %d examples for development purposes..." % MINI_SIZE
