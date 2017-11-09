cad682d7495ce960317e6a8c8dee035043170545
Janani Padmanabhan <aspire.janani@gmail.com>
2017-11-08 21:02:22 -0500

    Mimicking

--- SPINN/main.py
+++ SPINN/main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import torch
 import torch.optim as optim
+import torch.nn.functional as F
 from snli_classifier import SNLIClassifier
 from batcher import prepare_snli_batches
 import numpy as np
@@ -8,28 +9,40 @@ import argparse
 import sys
 from utils import render_args
 
+def get_l2_loss(model, l2_lambda):
+    loss = 0.0
+    for w in model.parameters():
+        if w.requires_grad:
+            loss += l2_lambda * torch.sum(torch.pow(w, 2))
+    return loss
+
 def predict(model, sent1, sent2, cuda=False):
     model.eval()
     output = model(sent1, sent2)
+    logits = F.log_softmax(output)
     if cuda:
-        return output.data.cpu().numpy().argmax(axis=1)
-    return output.data.numpy().argmax(axis=1)
+        return logits.data.cpu().numpy().argmax(axis=1)
+    return logits.data.numpy().argmax(axis=1)
 
 def train_batch(model, loss, optimizer, sent1, sent2, y_val):
     # Reset gradient
     optimizer.zero_grad()
     # Forward
     fx = model(sent1, sent2)
+    logits = F.log_softmax(fx)
 
-    output = loss.forward(fx, y_val)
+    total_loss = loss(logits, y_val)
+
+    total_loss += get_l2_loss(model, 1e-05)
+
     # Backward
-    output.backward()
+    total_loss.backward()
     for param in model.parameters():
-        if param.grad is not None:
+        if param.grad:
             param.grad.data.clamp(-5, 5)
     # Update parameters
     optimizer.step()
-    return output.data[0]
+    return total_loss.data[0]
 
 def train(args):
     print ("Starting...")
@@ -45,8 +58,8 @@ def train(args):
     sys.stdout.flush()
     if args.gpu > -1:
         model.cuda()
-    loss = torch.nn.CrossEntropyLoss(size_average=True)
-    optimizer = optim.Adagrad(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
+    loss = torch.nn.NLLLoss()
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001, betas=(0.9, 0.999), eps=1e-08)
     count_iter = 0
     train_iter.repeat = False
     for epoch in range(args.epochs):
@@ -127,7 +140,7 @@ if __name__=='__main__':
     parser.add_argument('--dropout_rate_input', type=float, default=0.1)
     parser.add_argument('--dropout_rate_classify', type=float, default=0.05)
     parser.add_argument('-no_batch_norm', action='store_true', default=False)
-    parser.add_argument('-gpu', type=int, action='store_true', default=-1)
+    parser.add_argument('-gpu', type=int, default=-1)
     args = parser.parse_args()
     render_args(args)
     sys.stdout.flush()
--- SPINN/snli_classifier.py
+++ SPINN/snli_classifier.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from spinn import SPINN
+from actions import HeKaimingInitializer, LayerNormalization
 
 class SNLIClassifier(nn.Module):
     def __init__(self, args, vocab_size):
@@ -11,24 +12,24 @@ class SNLIClassifier(nn.Module):
         self.softmax = nn.Softmax()
         self.relu = nn.ReLU()
 
-        self.batch_norm_mlp_input = nn.BatchNorm1d(4 * self.args.hidden_size)
-        self.batch_norm_mlp_hidden = nn.BatchNorm1d(self.args.snli_h_dim)
+        self.layer_norm_mlp_input = LayerNormalization(4 * self.args.hidden_size / 2)
+        self.layer_norm_mlp_hidden = LayerNormalization(self.args.snli_h_dim)
 
         self.dropout = nn.Dropout(p=self.args.dropout_rate_classify)
 
         self.mlp = []
         for i in range(self.args.snli_num_h_layers):
-            input_dim = 4 * self.args.hidden_size if i == 0 else self.args.snli_h_dim
+            input_dim = 4 * self.args.hidden_size / 2 if i == 0 else self.args.snli_h_dim
             out_dim = self.args.snli_h_dim
             if args.gpu > -1:
-                
                 self.mlp.append(nn.Linear(input_dim, out_dim).cuda())
             else:
                 self.mlp.append(nn.Linear(input_dim, out_dim))
+            HeKaimingInitializer(self.mlp[-1].weight)
 
         self.output = nn.Linear(self.args.snli_h_dim, 3)
-        self.premise = SPINN(self.args)
-        self.hypothesis = SPINN(self.args)
+        HeKaimingInitializer(self.output.weight)
+        self.spinn = SPINN(self.args)
 
     def set_weight(self, weight):
         self.embed.weight.data.copy_(torch.from_numpy(weight))
@@ -45,13 +46,13 @@ class SNLIClassifier(nn.Module):
         hyp_embed = self.embed(hypothesis[0])
         prem_embed = self.embed(premise[0])
 
-        hyp_encode = self.hypothesis(hyp_embed, hypothesis[1])
-        prem_encode = self.premise(prem_embed, premise[1])
+        hyp_encode = self.spinn(hyp_embed, hypothesis[1])
+        prem_encode = self.spinn(prem_embed, premise[1])
 
         features = self.prepare_features(hyp_encode, prem_encode)
 
-        if not self.args.no_batch_norm:
-            features = self.batch_norm_mlp_input(features)
+
+        features = self.layer_norm_mlp_input(features)
 
         if self.args.dropout_rate_classify > 0:
             features = self.dropout(features)
@@ -60,10 +61,9 @@ class SNLIClassifier(nn.Module):
             # ReLu plus weight matrix
             features = self.relu(layer(features))
             # batch norm
-            if not self.args.no_batch_norm:
-                features = self.batch_norm_mlp_hidden(features)
+            features = self.layer_norm_mlp_hidden(features)
             # dropout
             if self.args.dropout_rate_classify > 0:
                 features = self.dropout(features)
 
-        return self.softmax(self.output(features))
+        return self.output(features)
