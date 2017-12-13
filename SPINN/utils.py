73cbc59ea9f66154093eea679e9e6dd6b159b753
Griffin Adams <griff4692@gmail.com>
2017-12-13 15:48:18 -0500

    Fixes tree edit distance code.

--- SPINN/utils.py
+++ SPINN/utils.py
@@ -14,32 +14,39 @@ def cudify(args, tensor):
 def tree_edit(act1, act2):
 	tree1 = make_tree(act1)
 	tree2 = make_tree(act2)
-
 	return simple_distance(tree1, tree2)
 
 def make_tree(act):
 	sent_len = sum([1 if 'shift' else 0 for a in act])
-	buf = reversed(range(sent_len))
+	num_shifts = 0
 	stack = []
 
 	for a in act:
 		if a == 'shift':
-			word_idx = buf.pop()
-			new_node = Node(str(word_idx))
+			new_node = Node(str(num_shifts))
 			stack.append(new_node)
+			num_shifts += 1
 		elif a == 'reduce':
 			noder = stack.pop()
 			nodel = stack.pop()
 
-			lLabel = nodel.get_label()
-			rLabel = noder.get_label()
+			lLabel = Node.get_label(nodel)
+			rLabel = Node.get_label(noder)
 
 			joint = lLabel + "-" + rLabel
 
 			new_node = Node(joint)
-			new_node.addKid(nodel)
-			new_node.addKid(noder)
+			new_node.addkid(nodel)
+			new_node.addkid(noder)
+			stack.append(new_node)
 		else:
 			raise Exception("Should be either shift or reduce!")
 	assert len(stack) == 1
 	return stack.pop()
+
+if __name__ == '__main__':
+	a1 = ['shift', 'shift', 'shift', 'reduce', 'reduce']
+	a2 = ['shift', 'shift', 'reduce', 'shift', 'reduce']
+
+	d = tree_edit(a1, a2)
+	print(d)
